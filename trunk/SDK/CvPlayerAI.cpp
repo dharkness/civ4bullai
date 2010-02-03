b2a40728cf83873317f51c27d8723549733fa129
fim-fuyu <fim-fuyu@31ee56aa-37e8-4f44-8bdf-1f84a3affbab>
2010-02-03 10:06:27 +0000

    BULL r126 Makefile included (based on BULL makefile, untested)

--- trunk/SDK/CvPlayerAI.cpp
+++ trunk/SDK/CvPlayerAI.cpp
@@ -12073,6 +12073,10 @@ void CvPlayerAI::AI_changeMemoryCount(PlayerTypes eIndex1, MemoryTypes eIndex2, 
 	FAssertMsg(eIndex2 >= 0, "eIndex2 is expected to be non-negative (invalid Index)");
 	FAssertMsg(eIndex2 < NUM_MEMORY_TYPES, "eIndex2 is expected to be within maximum bounds (invalid Index)");
 	m_aaiMemoryCount[eIndex1][eIndex2] += iChange;
+	if (eIndex1 == GC.getGameINLINE().getActivePlayer())
+	{
+		gDLL->getInterfaceIFace()->setDirty(Score_DIRTY_BIT, true);
+	}
 	FAssert(AI_getMemoryCount(eIndex1, eIndex2) >= 0);
 }
 
